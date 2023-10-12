224acebee03595d7613e9503b5f49a7c6ce2cfcc
Yehuda Ringler <ybringler@gmail.com>
2023-10-12 11:38:25 -0400

    Remove not used config, update readme

--- dart/lib/config.dart
+++ dart/lib/config.dart
@@ -1,19 +1,13 @@
 import 'package:dotenv/dotenv.dart';
 
 class FlashConfig {
-  final bool downloadAudio;
   final int productId;
   final String language;
 
-  FlashConfig(
-      {required this.downloadAudio,
-      required this.productId,
-      required this.language});
+  FlashConfig({required this.productId, required this.language});
 }
 
 final env = DotEnv()..load();
 
 final config = FlashConfig(
-    downloadAudio: bool.parse(env['downloadAudio']!),
-    productId: int.parse(env['productId']!),
-    language: env['language']!);
+    productId: int.parse(env['productId']!), language: env['language']!);
